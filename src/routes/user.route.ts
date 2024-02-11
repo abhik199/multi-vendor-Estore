eea9c728bf5730e4cababc4b8cf8d26c1b63e23d
abhik199 <abhishekkirar2004@gmail.com>
2024-02-11 14:56:53 +0530

    add route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,7 +5,7 @@ const userRoute = Router();
 
 userRoute.post("/register", userController.create_user);
 userRoute.post("/login", userController.userLogin);
-userRoute.post("forgot_password");
+userRoute.post("forgot_password", userController.forgotPassword);
 userRoute.post("/change_password", userController.changePassword);
 userRoute.post("/verify_otp", userController.verifyUser);
 userRoute.post("/resend_email", userController.reVerify);
